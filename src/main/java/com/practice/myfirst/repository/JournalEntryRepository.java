406b22b04666afa016f70c5a34b1500fa560bc04
raj kadam <kadamraj623@gmail.com>
2024-09-26 19:52:54 +0530

    learn responseEntiry

---
+++ src/main/java/com/practice/myfirst/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.practice.myfirst.repository;
+
+import com.practice.myfirst.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
